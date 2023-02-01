01c3107f8baba90d8b6497613d7d2fca215817e8
Vijayasri Iyer <thisisvij98@gmail.com>
2023-02-01 22:28:58 +0530

    Modified imports

--- lib/model/blocks.py
+++ lib/model/blocks.py
@@ -1,20 +1,13 @@
+import math
 import torch.nn as nn
 import torch
-from torchvision.ops.boxes import nms as nms_torch
 import torch.nn.functional as F
-import math
-from functools import partial
 
 class SeparableConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels=None, norm=True, activation=False, onnx_export=False):
         super(SeparableConvBlock, self).__init__()
         if out_channels is None:
             out_channels = in_channels
-
-        # Q: whether separate conv
-        #  share bias between depthwise_conv and pointwise_conv
-        #  or just pointwise_conv apply bias.
-        # A: Confirmed, just pointwise_conv applies bias, depthwise_conv has no bias.
 
         self.depthwise_conv = Conv2dStaticSamePadding(in_channels, in_channels,
                                                       kernel_size=3, stride=1, groups=in_channels, bias=False)
@@ -99,80 +92,6 @@ class MergeBlock(nn.Module):
             raise ValueError(
                 "`merge_policy` must be one of: ['add', 'cat'], got {}".format(self.policy)
             )
-
-
-class BiFPNDecoder(nn.Module):
-    def __init__(
-            self,
-            encoder_depth=5,
-            pyramid_channels=64,
-            segmentation_channels=64,
-            dropout=0.2,
-            merge_policy="add", ):
-        super().__init__()
-
-        self.seg_blocks = nn.ModuleList([
-            SegmentationBlock(pyramid_channels, segmentation_channels, n_upsamples=n_upsamples)
-            for n_upsamples in [5,4, 3, 2, 1]
-        ])
-        
-        self.seg_p2 = SegmentationBlock(32, 64, n_upsamples=0)
-
-        self.merge = MergeBlock(merge_policy)
-
-        self.dropout = nn.Dropout2d(p=dropout, inplace=True)
-
-    def forward(self, inputs):
-        p2, p3, p4, p5, p6, p7 = inputs
-
-        feature_pyramid = [seg_block(p) for seg_block, p in zip(self.seg_blocks, [p7, p6, p5, p4, p3])]
-        
-        p2 = self.seg_p2(p2)
-            
-        p3,p4,p5,p6,p7 = feature_pyramid
-
-        x = self.merge((p2,p3,p4,p5,p6,p7))
-
-        x = self.dropout(x)
-
-        return x
-
-
-class Classifier(nn.Module):
-    def __init__(self, in_channels, num_anchors, num_classes, num_layers, pyramid_levels=5, onnx_export=False):
-        super(Classifier, self).__init__()
-        self.num_anchors = num_anchors
-        self.num_classes = num_classes
-        self.num_layers = num_layers
-        self.conv_list = nn.ModuleList(
-            [SeparableConvBlock(in_channels, in_channels, norm=False, activation=False) for i in range(num_layers)])
-        self.bn_list = nn.ModuleList(
-            [nn.ModuleList([nn.BatchNorm2d(in_channels, momentum=0.01, eps=1e-3) for i in range(num_layers)]) for j in
-             range(pyramid_levels)])
-        self.header = SeparableConvBlock(in_channels, num_anchors * num_classes, norm=False, activation=False)
-        self.swish = MemoryEfficientSwish() if not onnx_export else Swish()
-
-    def forward(self, inputs):
-        feats = []
-        for feat, bn_list in zip(inputs, self.bn_list):
-            for i, bn, conv in zip(range(self.num_layers), bn_list, self.conv_list):
-                feat = conv(feat)
-                feat = bn(feat)
-                feat = self.swish(feat)
-            feat = self.header(feat)
-
-            feat = feat.permute(0, 2, 3, 1)
-            feat = feat.contiguous().view(feat.shape[0], feat.shape[1], feat.shape[2], self.num_anchors,
-                                          self.num_classes)
-            feat = feat.contiguous().view(feat.shape[0], -1, self.num_classes)
-
-            feats.append(feat)
-
-        feats = torch.cat(feats, dim=1)
-        feats = feats.sigmoid()
-
-        return feats
-
 
 class Swish(nn.Module):
     def forward(self, x):
--- lib/model/heads.py
+++ lib/model/heads.py
@@ -1,6 +1,6 @@
 import torch 
 import torch.nn as nn 
-
+from lib.model.blocks import SegmentationBlock, MergeBlock
 
 class BiFPNDecoder(nn.Module):
     def __init__(
