6ae47e62ab3e65d86a77caa6400bd8d062a3f197
Vijayasri Iyer <thisisvij98@gmail.com>
2023-02-05 19:25:45 +0530

    Added classification head

--- lib/model/heads.py
+++ lib/model/heads.py
@@ -47,3 +47,15 @@ class DepthHead(nn.Sequential):
         activation = Activation(activation)
         super().__init__(conv2d, upsampling, activation)
 
+class ClassificationHead(nn.Sequential):
+    def __init__(self, in_channels, classes, pooling="avg", dropout=0.2, activation=None):
+        if pooling not in ("max", "avg"):
+            raise ValueError("Pooling should be one of ('max', 'avg'), got {}.".format(pooling))
+        pool = nn.AdaptiveAvgPool2d(1) if pooling == 'avg' else nn.AdaptiveMaxPool2d(1)
+        flatten = nn.Flatten()
+        dropout = nn.Dropout(p=dropout, inplace=True) if dropout else nn.Identity()
+        linear = nn.Linear(in_channels, classes, bias=True)
+        activation = Activation(activation)
+        super().__init__(pool, flatten, dropout, linear, activation)
+
+
